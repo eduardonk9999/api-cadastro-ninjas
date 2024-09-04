0fe5c309859bf142b4f3c68d02e06959c63474c2
eduardonk9999 <thiago.novak7@gmail.com>
2024-09-04 18:48:00 -0300

    feat: config inicial

---
+++ API-Ninjas/src/main/java/com/ninja/API/Ninjas/ApiNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.ninja.API.Ninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNinjasApplication.class, args);
+	}
+
+}
---
+++ API-Ninjas/src/test/java/com/ninja/API/Ninjas/ApiNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninja.API.Ninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
