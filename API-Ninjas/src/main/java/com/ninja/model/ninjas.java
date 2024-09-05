4290de1e80cabf80bd96c6e618251f29cf5375ef
eduardonk9999 <thiago.novak7@gmail.com>
2024-09-05 16:04:37 -0300

    feat: repository

--- API-Ninjas/src/main/java/com/ninja/model/ninjas.java
+++ API-Ninjas/src/main/java/com/ninja/model/ninjas.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+// MODEL QUE É O QUE VAI SER SALVO NO BD
 @Entity
 @Table(name = "ninjas")
 @Data // -> Com essa parada do lombk ele cria os getrs e setters sem precisar já sai declarando tudo no código
---
+++ API-Ninjas/src/main/java/com/ninja/repository/ninjasRepository.java
@@ -0,0 +1,6 @@
+package com.ninja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ninjasRepository extends JpaRepository {
+}
