ef7d7231afb8ea8b27a172e3d296214365d6d1ea
eduardonk9999 <thiago.novak7@gmail.com>
2024-09-05 12:42:40 -0300

    feat: class ninjas

--- API-Ninjas/src/main/java/com/ninja/model/ninjas.java
+++ API-Ninjas/src/main/java/com/ninja/model/ninjas.java
@@ -1,4 +1,21 @@
 package com.ninja.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ninjas")
+@Data // -> Com essa parada do lombk ele cria os getrs e setters sem precisar já sai declarando tudo no código
+@AllArgsConstructor // Mesmas coisa do de cima, mas para construtores
+@NoArgsConstructor
 public class ninjas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String nome;
+    String aldeia;
+    int idade;
+    String elemento;
+    String imgUrl;
 }
